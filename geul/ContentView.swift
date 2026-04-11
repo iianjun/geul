7920c30e8bc949407aac2a4b76e40cb082c289e1
Hasung Jun <59468036+iianjun@users.noreply.github.com>
2026-04-12 03:28:24 +0900

    project init

---
+++ geul/ContentView.swift
@@ -0,0 +1,8 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, Geul!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
---
+++ geul/GeulApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct GeulApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
